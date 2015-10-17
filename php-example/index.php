4da1d3564aea88faaaa15898f9e48b3b77d8a1ec
sevenmaster <seven-master@hotmail.de>
2015-10-17 15:10:35 +0000

    css examplepage

--- php-example/index.php
+++ php-example/index.php
@@ -1,13 +1,17 @@
 <html>
 	<head>
 		<title>Eventkarte</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<?php
-		define("EVENTKARTE_DB_PATH", realpath(dirname(__FILE__)) . "/eventkarte.db");
-		define("EVENTKARTE_LIB_URL", "http://localhost/Eventkarte/php-lib");
-		define("EVENTKARTE_EVENT_LOCATION", "48.7666667/9.1833333");
-		include("../php-lib/library.php");
-		?>
+		<div id="header"><h1>Eventkarte</h1></div>
+		<div id="content">
+			<?php
+			define("EVENTKARTE_DB_PATH", realpath(dirname(__FILE__)) . "/eventkarte.db");
+			define("EVENTKARTE_LIB_URL", "http://localhost/Eventkarte/php-lib");
+			define("EVENTKARTE_EVENT_LOCATION", "48.7666667/9.1833333");
+			include("../php-lib/library.php");
+			?>
+		</div>
 	</body>
 </html>
