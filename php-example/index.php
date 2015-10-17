65ee19fa49c3aa4af5c98092b40f80618be4ff28
sevenmaster <seven-master@hotmail.de>
2015-10-17 14:09:47 +0000

    started examplepage

--- php-example/index.php
+++ php-example/index.php
@@ -1,6 +1,13 @@
-<?php
-
-define("EVENTKARTE_DB_PATH", realpath(dirname(__FILE__)) . "/eventkarte.db");
-define("EVENTKARTE_LIB_URL", "http://localhost/Eventkarte/php-lib");
-define("EVENTKARTE_EVENT_LOCATION", "48.7666667/9.1833333");
-include("../php-lib/library.php");
+<html>
+	<head>
+		<title>Eventkarte</title>
+	</head>
+	<body>
+		<?php
+		define("EVENTKARTE_DB_PATH", realpath(dirname(__FILE__)) . "/eventkarte.db");
+		define("EVENTKARTE_LIB_URL", "http://localhost/Eventkarte/php-lib");
+		define("EVENTKARTE_EVENT_LOCATION", "48.7666667/9.1833333");
+		include("../php-lib/library.php");
+		?>
+	</body>
+</html>
