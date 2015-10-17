9182dc3eec747ef1cdfa0d41f24148f89f9ad8fe
ByteHamster <info@bytehamster.com>
2015-10-17 09:15:34 +0000

    Basic library setup

---
+++ php-example/index.php
@@ -0,0 +1,5 @@
+<?php
+
+define("EVENTKARTE_DB_LOCATION", "/Eventkarte/php-example/eventkarte.db");
+define("EVENTKARTE_BACKEND_LOCATION", "/Eventkarte/php-library/backend/");
+include("../php-lib/library.php");
---
+++ php-lib/library.php
@@ -0,0 +1,7 @@
+<?php
+
+if(!defined("EVENTKARTE_DB_LOCATION") || !defined("EVENTKARTE_BACKEND_LOCATION")) {
+	exit("Eventkarte: Please define EVENTKARTE_DB_LOCATION and EVENTKARTE_BACKEND_LOCATION");
+}
+
+echo "<div id=\"eventkarte\">Eventkarte...</div>";
