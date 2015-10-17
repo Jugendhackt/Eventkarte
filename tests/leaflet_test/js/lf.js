5fb69d7c61faa182e08b4901f00906e7c5a12d06
kaschunuss <kaschunuss26@googlemail.com>
2015-10-17 13:15:21 +0200

    polyline, maker, popup added

--- tests/leaflet_test/js/lf.js
+++ tests/leaflet_test/js/lf.js
@@ -3,4 +3,22 @@ var map = L.map('map').setView([51.5, -0.09], 15);
 // create the tile layer with correct attribution
 var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var markerA = L.marker([51.5, -0.09]).addTo(map);
+var markerB = L.marker([52.5170365, 13.3888599]).addTo(map);
+
+markerA.bindPopup("London");
+markerB.bindPopup("Berlin");
+
+var PosA = new L.LatLng(51.5, -0.09);
+var PosB = new L.LatLng(52.5170365, 13.3888599);
+var posList = [PosA, PosB];
+
+var polylineAB = new L.polyline(posList, {
+
+	color: 'green',
+	weight: 2,
+	opacity: 1,
+	smoothFactor: 1
+
+});
+polylineAB.addTo(map);
