647458c961e07ff68d8da1439ec9e1cf664b5520
blobfisch <nina-roeckelein@posteo.de>
2015-10-17 13:11:52 +0200

    JSON example

--- tests/leaflet_test/js/lf.js
+++ tests/leaflet_test/js/lf.js
@@ -4,3 +4,4 @@ var map = L.map('map').setView([51.5, -0.09], 15);
 var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
+marker.bindPopup("Hallo Welt").openPopup();
